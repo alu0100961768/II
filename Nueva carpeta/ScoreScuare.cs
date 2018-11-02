b89d63ebcf6fb76a5198afb57b0a45e59baaf8f5
alu0100961768 <32639399+alu0100961768@users.noreply.github.com>
2018-11-02 01:44:23 +0000

    II
    Mañana hago el informe

---
+++ Nueva carpeta/ScoreScuare.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+
+//public delegate void metodoDelegado (int paramentro);
+
+
+public class ScoreScuare : MonoBehaviour {
+
+
+	public static ScoreScuare instance;
+	public int count;
+	//public event metodoDelegado Evento;
+	//	private Score score;
+	// Use this for initialization
+
+	void Awake() {}
+
+	void Start () {instance = this;}
+	
+	// Update is called once per frame
+	void Update () {}
+
+	void OnTriggerEnter(Collider c){
+		if (c.gameObject.tag == "Score") {
+			GanarPuntos ();
+		}
+		if (c.gameObject.tag == "Fire") {
+			PerderPuntos ();
+		}
+	}
+
+
+	void GanarPuntos(){
+		count++;
+		Debug.Log ("Tienes " + count + " puntos.");
+		//count += recolecta;
+	}
+
+	void PerderPuntos(){
+		count = 0;
+		Debug.Log ("Tienes " + count + " puntos.");
+	}
+
+}
---
+++ Nueva carpeta/pared.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pared : MonoBehaviour {
+
+	public Material Color;
+
+	public delegate void metodoDelegado ();
+	public static event metodoDelegado Evento;
+
+
+	void OnTriggerEnter(Collider c){
+		Evento ();
+	}
+		
+	void cambiarcolor(){
+		this.gameObject.GetComponent<MeshRenderer> ().material = Color;
+	}
+
+	// Use this for initialization
+	void Start () {
+		//metodoDelegado = cambiarcolor ();
+	}
+	
+	// Update is called once per frame
+	void OnEnable(){
+		Evento += cambiarcolor;
+	}
+
+	void OnDisable()
+	{
+		Evento -= cambiarcolor;
+	}
+
+}
